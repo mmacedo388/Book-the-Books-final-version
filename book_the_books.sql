c201af33b559e4be0db456323bde70ebbd33e4c3
mmacedo388 <34384412+mmacedo388@users.noreply.github.com>
2023-03-10 19:42:55 +0000

    Update book_the_books.sql

--- book_the_books.sql
+++ book_the_books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09-Nov-2022 às 22:21
+-- Tempo de geração: 10-Mar-2023 às 20:41
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -24,21 +24,112 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `catalog`
+--
+
+CREATE TABLE `catalog` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `catalog`
+--
+
+INSERT INTO `catalog` (`id`, `name`, `price`, `desc`, `img`, `quantity`) VALUES
+(2, 'olá', '123', 'olá', '2023.01.22-18.35.25.jpg', '1'),
+(3, 'o sorriso das estrelas', '456', 'olá', '2023.01.22-18.56.30.jpg', '1'),
+(4, 'adeus', '789', 'arcadia', '2023.01.22-18.57.16.jpg', '1'),
+(5, 'fedsdgs', '123', 'adsasfdesa', '2023.01.22-18.57.47.jpg', '1'),
+(6, 'edfgdhaf', '456', 'sadfrsag', '2023.01.22-19.00.55webp', '1'),
+(7, 'fgrew', '45', 'wefsdewsd', '2023.01.22-19.01.13.jpg', '1'),
+(8, '3qwerffdsafrds', '1321', 'dsafdsaf', '2023.01.22-19.01.51jpeg', '1'),
+(10, 'adssada', '123213', 'asfqewad', '2023.01.27-19.11.22.jpg', '1'),
+(11, 'asdsafd', '21321', 'asdqwasas', '2023.01.27-21.00.16jpeg', '1'),
+(12, 'dfssed', '123 12', 'faa', '2023.01.28-20.19.13.jpg', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `form`
 --
 
 CREATE TABLE `form` (
-  `id` bigint(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  `age` int(3) NOT NULL,
-  `message` varchar(5000) NOT NULL,
-  `registration_date` date NOT NULL
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `registration_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `form`
+--
+
+INSERT INTO `form` (`id`, `name`, `email`, `gender`, `age`, `message`, `registration_date`) VALUES
+(1, 'Miguel Macedo', 'mmacedo388@hotmail.com', 'M', '0-29', 'Hi!', '2023-01-14 18:17:24'),
+(3, 'Miguel Macedo', 'mmacedo388@hotmail.com', 'M', '0-29', 'Hi!\r\n', '2023-02-04 17:42:44'),
+(4, 'Miguel Macedo', 'mmacedo388@hotmail.com', 'M', '0-29', 'Hi!\r\n', '2023-02-04 17:42:46');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `admin` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `user`, `email`, `password`, `admin`) VALUES
+(1, 'Miguel de Portugal', 'mmacedo388@hotmail.com', '123', 1),
+(2, 'Ninguém', 'nayron@gmail.com', '123', 0);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `catalog`
+--
+ALTER TABLE `catalog`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `form`
@@ -47,14 +138,38 @@ ALTER TABLE `form`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `user`
+--
+ALTER TABLE `user`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT de tabela `catalog`
+--
+ALTER TABLE `catalog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `form`
 --
 ALTER TABLE `form`
-  MODIFY `id` bigint(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
