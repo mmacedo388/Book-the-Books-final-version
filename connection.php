1b82e6bbd4a8c2cfeaf23b97e54380230f2883cf
António Azevedo <antonio.v.azevedo@gmail.com>
2023-03-10 20:19:19 +0000

    format code

--- connection.php
+++ connection.php
@@ -5,6 +5,5 @@ $username = "root";
 $password = "";
 $dbname = "book_the_books";
 
-   //Create a connection
-   $dbc = mysqli_connect($hostname, $username, $password, $dbname);
-?>
+//Create a connection
+$dbc = mysqli_connect($hostname, $username, $password, $dbname);
--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php
-include ('connection.php');
+
+include('connection.php');
 session_start();
-if($_SESSION){
-$user_id = $_SESSION['user_id'];
-}
-else{
-	$user_id = "";
+if ($_SESSION) {
+    $user_id = $_SESSION['user_id'];
+} else {
+    $user_id = "";
 }
 
 $name = $_POST['name'];
@@ -14,29 +14,18 @@ $desc = $_POST['desc'];
 
 
 //upload start
- if(isset($_FILES['pic']))
- {
-    $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
+if (isset($_FILES['pic'])) {
+    $ext = strtolower(substr($_FILES['pic']['name'], -4)); //Pegando extensão do arquivo
     $new_name = date("Y.m.d-H.i.s").$ext; //Definindo um novo nome para o arquivo
-    $dir = "C:/xampp/htdocs/site/images/"; //Diretório para uploads 
+    $dir = __DIR__ ."/images/"; //Diretório para uploads
     move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
     echo("Imagen enviada com sucesso!");
     $dir_name = $new_name;
- }
- //upload finish
- if($name && $price && $desc && $dir_name){
-
-   mysqli_query( $dbc, "INSERT INTO catalog (`name`, `price`, `desc`, `img`, `quantity`) VALUES ('$name','$price','$desc','$dir_name', '1');");
-   header("Location: catalog.php");
-
- }else{
-  
-   header("Location: products_admin.php");
-
- }
-
- 
-
- 
-
-?>
+}
+//upload finish
+if ($name && $price && $desc && $dir_name) {
+    mysqli_query($dbc, "INSERT INTO catalog (`name`, `price`, `desc`, `img`, `quantity`) VALUES ('$name','$price','$desc','$dir_name', '1');");
+    header("Location: catalog.php");
+} else {
+    header("Location: products_admin.php");
+}
