7ac02c4af8d33ac3df539dd61d050d37a7310ee0
mmacedo388 <mmacedo388@hotmail.com>
2023-04-19 11:12:17 +0100

    close button modal cart

--- cart/modal.php
+++ cart/modal.php
@@ -1,13 +1,13 @@
 <?php
-require(dirname(__DIR__).'/connection.php');
+require(dirname(__DIR__) . '/connection.php');
 
 $cart = $_SESSION['cart'] ?? null;
 
-if ($cart):
+$productIds = array_map(function ($line) {
+    return $line['product_id'];
+}, $cart['lines'] ?? []);
 
-    $productIds = array_map(function ($line) {
-        return $line['product_id'];
-    }, $cart['lines']);
+if ($productIds) :
 
     $productIds = implode(", ", $productIds);
     $result = mysqli_query($dbc, "SELECT * FROM catalog WHERE id IN ($productIds)");
@@ -22,71 +22,68 @@ if ($cart):
         $id = $line['product_id'];
         $cartTotal += ((float) $products[$id]['price']) * ((int) $line['quantity']);
     }
-    ?>
+?>
 
-<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header border-bottom-0">
-                <h5 class="modal-title" id="exampleModalLabel">
-                    Your Shopping Cart
-                </h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <table class="table table-image">
-                    <thead>
-                        <tr>
-                            <th scope="col"></th>
-                            <th scope="col">Product</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Qty</th>
-                            <th scope="col">Total</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+    <div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header border-bottom-0">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                        Your Shopping Cart
+                    </h5>
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-image">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">Product</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Qty</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
 
-                        <?php
-                            foreach ($cart['lines'] as $line):
+                            <?php
+                            foreach ($cart['lines'] as $line) :
 
                                 $productId = $line['product_id'];
-                                ?>
-                        <tr>
-                            <td class="w-25">
-                                <img src="/images/<?php echo $products[$productId]['img'] ?>" class="img-fluid img-thumbnail" alt="<?php echo $products[$productId]['name'] ?>">
-                            </td>
-                            <td><?php echo $products[$productId]['name'] ?></td>
-                            <td><?php echo $products[$productId]['price'] ?>&euro;</td>
-                            <!-- <?php echo $products[$productId]['description']; ?> -->
-                            <td class="qty"><input type="text" class="form-control" name="quantity" value="<?php echo $line['quantity'] ?>"></td>
-                            <td><?php echo((float)$products[$productId]['price']) * ((int)$line['quantity']) ?>&euro;</td>
-                            <td>
-                                <form action="/cart/delete-product.php" method="post">
-                                    <input type="hidden" name="page" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
-                                    <input type="hidden" name="id" value="<?php echo $productId ?>" />
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                            <i class="fa fa-times"></i>
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
-                        <?php endforeach ?>
-                    </tbody>
-                </table>
-                <div class="d-flex justify-content-end">
-                    <h5>Total: <span class="price text-success"><?php echo $cartTotal ?>&euro;</span></h5>
+                            ?>
+                                <tr>
+                                    <td class="w-25">
+                                        <img src="/images/<?php echo $products[$productId]['img'] ?>" class="img-fluid img-thumbnail" alt="<?php echo $products[$productId]['name'] ?>">
+                                    </td>
+                                    <td><?php echo $products[$productId]['name'] ?></td>
+                                    <td><?php echo $products[$productId]['price'] ?>&euro;</td>
+                                    <td class="qty"><input type="text" class="form-control" name="quantity" value="<?php echo $line['quantity'] ?>"></td>
+                                    <td><?php echo ((float)$products[$productId]['price']) * ((int)$line['quantity']) ?>&euro;</td>
+                                    <td>
+                                        <form action="/cart/delete-product.php" method="post" class="delete_button">
+                                            <input type="hidden" name="page" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
+                                            <input type="hidden" name="id" value="<?php echo $productId ?>" />
+                                            <button type="submit" class="btn btn-danger btn-sm">
+                                                <i class="fa fa-times"></i>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                    <div class="d-flex justify-content-end">
+                        <h5>Total: <span class="price text-success"><?php echo $cartTotal ?>&euro;</span></h5>
+                    </div>
                 </div>
-            </div>
-            <div class="modal-footer border-top-0 d-flex justify-content-between">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-success">Checkout</button>
+                <div class="modal-footer border-top-0 d-flex justify-content-between">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-success">Checkout</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
-
 <?php endif ?>
--- header-wrapper.php
+++ header-wrapper.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 if (isset($_SESSION['user_id'])) {
-    $user_admin = $_SESSION['user_admin'];
-    $user_name = $_SESSION['user_name'];
+	$user_admin = $_SESSION['user_admin'];
+	$user_name = $_SESSION['user_name'];
 } else {
-    $user_name = "";
-    $user_admin = "";
+	$user_name = "";
+	$user_admin = "";
 }
 
 ?>
@@ -13,32 +13,32 @@ if (isset($_SESSION['user_id'])) {
 	<div id="header" class="container">
 		<div id="menu">
 			<ul>
-		    	<a href="./index.php" title="Homepage">
+				<a href="./index.php" title="Homepage">
 					<li>Homepage</li>
-			    </a>
-
-                <a href="./catalog.php" title="Catalog">
-				<li>Catalog</li>
 				</a>
-                <a href="./about_us.php" title="About Us">
-				<li>About Us</li>
+
+				<a href="./catalog.php" title="Catalog">
+					<li>Catalog</li>
+				</a>
+				<a href="./about_us.php" title="About Us">
+					<li>About Us</li>
 				</a>
 				<a href="./contact_us.php" title="Contact Us">
-				<li>Contact Us</li>
+					<li>Contact Us</li>
 				</a>
 
 				<form id="search" action="/catalog.php" method="get">
 					<input type="search" class="form-control" name="q" value="<?php echo strip_tags(addslashes($_GET['q'] ?? '')) ?>" />
 					<button type="submit" id="search-btn" class="btn btn-primary"><i class="bi bi-search"></i></button>
 				</form>
-                
-				<?php if ($user_admin == "1") { ?>
-                 
-				<a href="./admin.php" title="Admin">	
-				<li>Admin</li>
-				</a>
 
-                <?php } ?>
+				<?php if ($user_admin == "1") { ?>
+
+					<a href="./admin.php" title="Admin">
+						<li>Admin</li>
+					</a>
+
+				<?php } ?>
 
 			</ul>
 		</div>
@@ -46,26 +46,26 @@ if (isset($_SESSION['user_id'])) {
 	<div id="menu2">
 		<ul>
 			<?php if (!$user_name) { ?>
-			<a href="/register"><i class="bi bi-r-circle-fill"></i>
-			<li>Register</li>
-			</a>
+				<a href="/register"><i class="bi bi-r-circle-fill"></i>
+					<li>Register</li>
+				</a>
 			<?php } ?>
 			<?php if ($user_name) { ?>
-			<a>Welcome, <?php echo $user_name ?> </a>
+				<a>Welcome, <?php echo $user_name ?> </a>
 			<?php } ?>
 			<?php if (!$user_name) { ?>
-			<a href="/login"><i class="bi bi-person-fill" ></i>
-			<li>Login</li>
-			</a>
+				<a href="/login"><i class="bi bi-person-fill"></i>
+					<li>Login</li>
+				</a>
 			<?php } ?>
 
-			<?php if (isset($_SESSION['cart'])): ?>
-			<button data-bs-toggle="modal" data-bs-target="#cartModal">
-				<i class="bi bi-cart-fill" ></i>
-				Cart
-			</button>
+			<?php if (isset($_SESSION['cart'])) : ?>
+				<button data-bs-toggle="modal" data-bs-target="#cartModal">
+					<i class="bi bi-cart-fill"></i>
+					Cart
+				</button>
 			<?php endif ?>
-			
+
 			<?php if ($user_name) { ?>
 				<form action="/login/logout.php" method="post" id="logout-form">
 					<button type="submit">
@@ -76,9 +76,9 @@ if (isset($_SESSION['user_id'])) {
 			<?php } ?>
 		</ul>
 	</div>
-		<div id="logo">
-			<img src="./images/logo.png" style="width: 225px!important";">
-		</div>
+	<div id="logo">
+		<img src="./images/logo.png" style="width: 225px!important" ;">
 	</div>
+</div>
 </div>
 <?php include('cart/modal.php') ?>
